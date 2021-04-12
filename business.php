ab34f374517cdec985dcb4992b9ca7fc28c95cbb
Blackya-de <yacinegauchi@gmail.com>
2021-04-12 18:51:15 +0100

    x44

---
+++ business.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Find-Go Business</title>
+    <link rel="stylesheet" href="CSS/search.css">
+    <link rel="stylesheet" href="CSS/business.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/1c84e8a5c3.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <header>
+      <div class="nav-bar">
+        <div class="nav-bar-header">
+          <div class="logo">
+            <h3>Find,Go</h3>
+          </div>
+          <div class="search-bar">
+            <div class="search-container">
+              <input type="text" name="" placeholder="Search">
+              <a href="#"> <li class="fa fa-search"></li> </a>
+            </div>
+          </div>
+          <div id="for-business">
+            <a href="#">For Businesses</a>
+            <a href="#">For Clients</a>
+          </div>
+          <div class="login-sigup-button">
+            <a href="login.html" class="btn">Login</a>
+            <a href="signup.html" class="btn" id="SignUp">SignUp</a>
+          </div>
+        </div>
+      </div>
+      <div class="header-descr">
+        <h1>Commencez dès maintenant à gérer votre profil d'établissement sur</br> Find Go .</h1>
+        <button type="button" name="button">Gérer mon business</button>
+      </div>
+    </header>
+    <section class="first">
+      <div class="section-text">
+        <h2>Attirez de nouveaux clients grâce à un profil d'établissement attrayant</h1>
+        <p>Vous pouvez publier des photos et des offres dans votre profil afin de
+          mettre en avant les spécificités de votre établissement,
+          et d'inciter les utilisateurs à vous choisir systématiquement.</p>
+      </div>
+      <div class="section-image">
+        <img src="img/business.svg" alt="" width="280px" height="200px">
+      </div>
+    </section>
+    <section class="second">
+      <div class="section-image">
+        <img src="img/Confirmed.svg" alt="" width="280px" height="200px">
+      </div>
+      <div class="section-text">
+        <h2>Proposez différentes solutions pour vous contacter</h1>
+        <p>Vos clients cherchent sur Google comment communiquer avec vous, que ce soit par téléphone,
+           par message ou en publiant des avis. Aujourd'hui,les méthodes de communications
+            se multiplient, ce qui peut vous permettre de développer votre activité.</p>
+      </div>
+    </section>
+  </body>
+</html>
---
+++ search.php
@@ -0,0 +1,198 @@
+<?php
+$user = 'yacine';
+$pass = "password";
+
+$db = new PDO('mysql:host=localhost;dbname=FINDGO' , $user , $pass ,array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION));
+$reponse = $db->query('SELECT * FROM business');
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="CSS/search.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/1c84e8a5c3.js" crossorigin="anonymous"></script>
+    <title>Search</title>
+  </head>
+  <body>
+    <header>
+      <div class="nav-bar">
+        <div class="nav-bar-header">
+          <div class="logo">
+            <h3>Find,Go</h3>
+          </div>
+          <div class="search-bar">
+            <div class="search-container">
+              <input type="text" name="" placeholder="Search">
+              <a href="#"> <li class="fa fa-search"></li> </a>
+            </div>
+          </div>
+          <div id="for-business">
+            <a href="business.php">For Businesses</a>
+            <a href="#">For Clients</a>
+          </div>
+          <div class="login-sigup-button">
+            <a href="login.html" class="btn">Login</a>
+            <a href="signup.html" class="btn" id="SignUp">SignUp</a>
+          </div>
+        </div>
+        <div class="nav-bar-menu">
+          <div class="dropdown">
+            <a href="#">Restaurant</a>
+            <span style="font-size: 14px; color: Dodgerblue;">
+              <i class="fas fa-chevron-down"></i>
+            </span>
+            <div class="dropdown-content">
+              <a href="#">Fast-Food</a>
+              <a href="#">Traditionnelle</a>
+              <a href="#">Livraison</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <a href="#">Home Services</a>
+            <span style="font-size: 14px; color: Dodgerblue;">
+              <i class="fas fa-chevron-down"></i>
+            </span>
+            <div class="dropdown-content">
+              <a href="#">Plombier</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <a href="#">Auto Services</a>
+            <span style="font-size: 14px; color: Dodgerblue;">
+              <i class="fas fa-chevron-down"></i>
+            </span>
+            <div class="dropdown-content">
+              <a href="#">Plombier</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <a href="#">More</a>
+            <span style="font-size: 14px; color: Dodgerblue;">
+              <i class="fas fa-chevron-down"></i>
+            </span>
+            <div class="dropdown-content">
+              <a href="#">Plombier</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="Search">
+        <div class="filter-section">
+          <h2>Filters</h2>
+          <div class="filter-options">
+            <h2>Features</h2>
+            <form class="" action="index.html" method="post">
+              <label class="container">Open Now
+                <input type="checkbox" >
+                <span class="checkmark"></span>
+              </label>
+              <label class="container">For Groups
+                <input type="checkbox">
+                <span class="checkmark"></span>
+              </label>
+              <label class="container">For Kids
+                <input type="checkbox">
+                <span class="checkmark"></span>
+              </label>
+              <label class="container">Outedoors places
+                <input type="checkbox">
+                <span class="checkmark"></span>
+              </label>
+            </form>
+            <a href="#" id="See-all">See all</a>
+          </div>
+          <div class="filter-options">
+            <h2>Wilaya</h2>
+            <form class="" action="index.html" method="post">
+              <label class="container">Alger
+                <input type="checkbox">
+                <span class="checkmark"></span>
+              </label>
+              <label class="container">Tizi Ouzou
+                <input type="checkbox">
+                <span class="checkmark"></span>
+              </label>
+              <label class="container">Oran
+                <input type="checkbox">
+                <span class="checkmark"></span>
+              </label>
+              <label class="container">Sétif
+                <input type="checkbox">
+                <span class="checkmark"></span>
+              </label>
+            </form>
+            <a href="#" id="See-all">See all</a>
+          </div>
+        </div>
+        <div class="search-content">
+          <div class="search-desc">
+            <h2 id="search-title">Résultat pour Tizi Ouzou</h2>
+            <div class="sort">
+              <div class="dropdown">
+                <a href="#">Recommandé</a>
+                <span style="font-size: 14px; color: Dodgerblue;">
+                  <i class="fas fa-chevron-down"></i>
+                </span>
+                <div class="dropdown-content">
+                  <a href="#">Plus Visité</a>
+                  <a href="#">Plus liké</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <?php
+            $i = 1;
+            while($donnes = $reponse->fetch())
+             {
+            ?>
+            <div class="box-container">
+            <div class="image-container">
+              <img src=<?php echo $donnes['img'] ?> alt="">
+            </div>
+            <div class="info-container">
+              <div class="profile-info">
+                <h2><a href="#"><?php echo $i.'.'.$donnes['nom']; ?></a></h2>
+                <div class="stars">
+                  <p><?php echo $donnes['NbrLike']; ?></p>
+                  <span style="font-size: 18px; color: Dodgerblue;">
+                    <i class="fas fa-star"></i>
+                  </span>
+                </div>
+              </div>
+              <?php echo '<p>' .$donnes['cat'] .'</p>'?>
+              <div id="location">
+                <span style="font-size: 20px; color: Dodgerblue;">
+                  <i class="fas fa-map-marker-alt"></i>
+                </span>
+                <a href="#"> <?php echo $donnes['adr'] ?>.</a>
+              </div>
+              <p id="info-text"><span>&ldquo;</span> <?php echo $donnes['descr'] ?> .
+                  <span>&ldquo;</span></p>
+            </div>
+          </div>
+            <?php
+            $i++;
+            }
+            ?>
+        </div>
+      </section>
+    </header>
+    <footer>
+      <div class="About">
+
+      </div>
+    </footer>
+    </div>
+  </body>
+</html>
+<<?php
+  echo '<p>'.$donnes['nom'].'</p>'
+ ?>
